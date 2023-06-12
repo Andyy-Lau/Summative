ca051b4a2537b83999ba2b0435f026f8f0a35843
Andyy-Lau <2005andyl@gmail.com>
2023-06-12 10:18:55 -0400

    repo init

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1ZRHBYRS-4wjp3RjylnWRvGdxmTkQ2vY",
+  authDomain: "summative-d5974.firebaseapp.com",
+  projectId: "summative-d5974",
+  storageBucket: "summative-d5974.appspot.com",
+  messagingSenderId: "1019483152388",
+  appId: "1:1019483152388:web:c6c99592cd3b26245a1551"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(config);
+export const firestore = getFirestore(config);
